c02da0b435c278dd482d4913461f9051cb64e2bf
Johan Winther <johan.winther@outlook.com>
2018-12-18 15:40:51 +0100

    Events checkbox

--- methods/metaboxes/vegas_selected_metabox.php
+++ methods/metaboxes/vegas_selected_metabox.php
@@ -2,8 +2,11 @@
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
         global $post;
         $enabled = get_post_meta( get_the_ID(), 'vegas-enabled', true );
+        $events_enabled = get_post_meta( get_the_ID(), 'vegas-events-enabled', true );
  ?>
 <form name="post" method="post">
         <input type="checkbox" name="vegas_selected_checkbox" id="vegas_selected_checkbox" <?= $enabled?'checked':'' ?> />
         <label for="vegas_selected_checkbox">Select as default</label>
+        <input type="checkbox" name="vegas_events_checkbox" id="vegas_events_checkbox" <?= $events_enabled?'checked':'' ?> />
+        <label for="vegas_events_checkbox">Include event images</label>
 </form>
